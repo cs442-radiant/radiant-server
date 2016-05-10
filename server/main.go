95902fef292e3a0acee50bd8b075657a5f9771e3
fiil12 <fiil12@hotmail.com>
2016-05-10 14:06:18 +0900

    Fix formatting

--- server/main.go
+++ server/main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-    "database/sql"
-    "log"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 func main() {
-    db, err := sql.Open("mysql", "radiant:radiant@tcp(ec2-54-191-70-38.us-west-2.compute.amazonaws.com:3306)/radiant")
-    
-    if err != nil {
-        log.Fatal(err)
-    } else {
-        log.Print("SQL connection")
-    }
-    
-    err = db.Ping()
-    if err != nil {
-        log.Fatal(err)
-    } else {
-        log.Print("Connection success")
-    
-        var id int
-        rows, err := db.Query("select * from Record")
-        if err != nil {
-            log.Fatal(err)
-        } else {
-            for rows.Next() {
-                err := rows.Scan(&id)
-                if err != nil {
-                    log.Fatal(err)
-                }
-                log.Println(id)
-            }    
-        }
-    }
-    
-    defer db.Close()
+	db, err := sql.Open("mysql", "radiant:radiant@tcp(ec2-54-191-70-38.us-west-2.compute.amazonaws.com:3306)/radiant")
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Print("SQL connection")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Print("Connection success")
+
+		var id int
+		rows, err := db.Query("select * from Record")
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			for rows.Next() {
+				err := rows.Scan(&id)
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Println(id)
+			}
+		}
+	}
+
+	defer db.Close()
 }
