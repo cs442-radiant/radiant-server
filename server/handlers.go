53531fff3777f63cef22ad23dcd0d08b87a70d4d
fiil12 <fiil12@hotmail.com>
2016-05-18 00:51:04 +0900

    Add GetBundle

--- server/handlers.go
+++ server/handlers.go
@@ -24,7 +24,7 @@ func GetRestaurant(w http.ResponseWriter, r *http.Request) {
 		var id int
 		var exists bool = false
 
-		type response struct {
+		type Response struct {
 			Exists bool `json:"exists"`
 		}
 
@@ -36,7 +36,7 @@ func GetRestaurant(w http.ResponseWriter, r *http.Request) {
 				fmt.Fprintln(w, err)
 			}
 
-			if err := json.NewEncoder(w).Encode(response{Exists: true}); err != nil {
+			if err := json.NewEncoder(w).Encode(Response{Exists: true}); err != nil {
 				log.Fatal(err)
 			}
 			exists = true
@@ -45,7 +45,7 @@ func GetRestaurant(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if !exists {
-			if err := json.NewEncoder(w).Encode(response{Exists: false}); err != nil {
+			if err := json.NewEncoder(w).Encode(Response{Exists: false}); err != nil {
 				log.Fatal(err)
 			}
 		}
@@ -53,9 +53,102 @@ func GetRestaurant(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetBundle(w http.ResponseWriter, r *http.Request) {
-	database.Query("SELECT * FROM ")
+	type Request struct {
+		RestaurantName    string `json:"restaurantName"`
+		BundleDescription string `json:"bundleDescription"`
+	}
 
-	fmt.Fprintln(w, "Get Bundle!")
+	var request Request
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		fmt.Fprintln(w, err)
+		log.Fatal(err)
+		return
+	}
+
+	rows, err := database.Query(fmt.Sprintf("SELECT id FROM Restaurant WHERE name = \"%s\"", request.RestaurantName))
+	defer rows.Close()
+
+	if err != nil {
+		fmt.Fprintln(w, err)
+		log.Fatal(err)
+		return
+	}
+
+	var restaurantId int
+
+	if rows.Next() {
+		rows.Scan(&restaurantId)
+	} else {
+		log.Println("No such restaurant exists... creating new restaurant")
+		rows, err := database.Query("SELECT MAX(id) FROM Restaurant")
+
+		if err != nil {
+			fmt.Fprintln(w, err)
+			log.Fatal(err)
+			return
+		}
+
+		var max int = 0
+
+		if rows.Next() {
+			rows.Scan(&max)
+		}
+
+		defer rows.Close()
+
+		{
+			var newRestaurantId = max + 1
+
+			_, err := database.Query(
+				fmt.Sprintf("INSERT INTO Restaurant VALUES(%d, \"%s\")", newRestaurantId, request.RestaurantName),
+			)
+
+			if err != nil {
+				fmt.Fprintln(w, err)
+				log.Fatal(err)
+				return
+			}
+
+			restaurantId = newRestaurantId
+		}
+	}
+
+	{
+		rows, err := database.Query("SELECT MAX(id) FROM Bundle")
+
+		if err != nil {
+			fmt.Fprintln(w, err)
+			log.Fatal(err)
+			return
+		}
+
+		var max int = 0
+
+		if rows.Next() {
+			rows.Scan(&max)
+		}
+
+		{
+			var newBundleId = max + 1
+
+			_, err := database.Query(
+				fmt.Sprintf("INSERT INTO Bundle VALUES(%d, %d, \"%s\")", newBundleId, restaurantId, request.BundleDescription),
+			)
+
+			if err != nil {
+				fmt.Fprintln(w, err)
+				log.Fatal(err)
+				return
+			}
+
+			type Response struct {
+				BundleId int `json:"bundleId"`
+			}
+
+			json.NewEncoder(w).Encode(Response{BundleId: newBundleId})
+		}
+	}
 }
 
 func PostSample(w http.ResponseWriter, r *http.Request) {
